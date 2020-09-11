1041b32481115a5c446950d5241650ba4640e09d
StounhandJ <rmarinicev@gmail.com>
2020-09-11 21:37:51 +0300

    v2 test

--- v2/db.py
+++ v2/db.py
@@ -4,4 +4,8 @@ class DataBase(object):
 	def __init__(self, arg):
 		con = pymysql.connect('localhost', 'root','7157725Rom', 'mpt')
     	cursor = con.cursor()
+	
+	def request(sql,date):
+		pass
+		
 		
